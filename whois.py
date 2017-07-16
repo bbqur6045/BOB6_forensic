cacd808e2601ef37c9d6f4eee10394601bbeb398
moon7392 <moon7392@naver.com>
2017-07-16 21:56:15 +0900

    Add whois.py

---
+++ whois.py
@@ -0,0 +1,19 @@
+import sys
+import whoisuse
+import json
+
+def date_handler(obj):
+	if hashttr(obj, 'isoformat'):
+		return obj.iosformat()
+	else:
+		raise TypeError
+num = sys.argv[1]
+f=open(sys.argv[2], 'r')
+for x in range(int(num)):
+	data = f.readline()
+	print whoisuse.get_whois(str(data))
+	json_dump = json.dumps(data, default=date_handler)
+	f_result = open("resultwhoisdata.txt", 'w')
+	f_result.write(whoisuse.get_whois(str(data)))
+f.close()
+	
